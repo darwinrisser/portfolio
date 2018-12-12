f331a36ed384e9b8e0c2d152edc5ae4b65ad4a78
Darwin <risser.darwin@gmail.com>
2018-12-11 18:14:40 -0600

    init commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,70 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import SimpleAppBar from './SimpleAppBar.js';
+import TitlebarGridList from './TitlebarGridList.js';
+import TitlebarGridList1 from './TitlebarGridList1.js';
+import SkillBar from 'react-skillbars';
+
+const skills = [
+  {type: "HTML", level: 85},
+  {type: "CSS", level: 85},
+  {type: "Javascript", level: 50},
+  {type: "React", level: 50},
+  {type: "JQuery", level: 25},
+  {type: "AfterEffects", level: 65},
+  {type: "Photoshop", level: 55},
+  {type: "UI/UX", level: 50}  
+];
+
+const colors = {
+  "bar": "#3498db",
+  "title": {
+  "text": "#fff",
+  "background": "#2980b9"
+  }
+}
 
 class App extends Component {
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <div id="nav">
+          <SimpleAppBar/>      
+        </div>
+        
+        <div id="header">
+          <h1>Darwin Risser</h1>
+          <h2>full-stack web developer from Austin, Texas</h2>
+          <div id="social">
+            <a href="https://github.com/drisser"><i className="fab fa-github"></i></a>
+            <a href="https://www.linkedin.com/in/darwinrisser/"><i className="fab fa-linkedin"></i></a>
+          </div>
+        </div>
+
+        <div id="skills-header">
+          <h1>About</h1>
+        </div>
+
+        <div id="skills">
+          
+          <div id="skillbar"><SkillBar skills={skills} colors={colors}/></div>
+          
+        </div>
+        <div id="portfolio-header">
+          <h1>Work</h1>
+          <p>Most of these projects were created for Austin Coding Academy. The rest are personal projects.</p>
+        </div>
+        <div id="portfolio">
+          <TitlebarGridList/> <TitlebarGridList1/>
+        </div>
+
+        <div id="contact">
+          <h1>Contact</h1>
+          
+        </div>
+
+
       </div>
     );
   }
---
+++ src/tileData1.js
@@ -0,0 +1,25 @@
+const tileData1 = [
+    {
+      img: "https://i.imgur.com/NAw8OoG.jpg",
+      title: 'Jeff Lowrance Designs',
+      author: 'Darwin Risser'
+    },
+    {
+      img: "https://i.imgur.com/auh02Re.png",
+      title: 'Blue VPN',
+      author: 'Darwin Risser'
+    },
+    {
+        img: "https://i.imgur.com/3U6SSMG.png",
+        title: 'Bitcoin Miner',
+        author: 'Darwin Risser'
+      },
+      {
+        img: "https://i.imgur.com/cFrUTOx.jpg",
+        title: 'Resume',
+        author: 'Darwin Risser'
+      }
+
+ ]
+
+ export default tileData1;
